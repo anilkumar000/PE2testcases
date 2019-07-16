4451b56dc2adbcce30b748a5780921eca5f246ad
sushree mishra <sushree022@gmail.com>
2019-07-16 18:08:53 +0530

    logic for aggregate finder added

--- src/main/java/com/stackroute/PE2/com/stackroute/PE2/AggregateFinder.java
+++ src/main/java/com/stackroute/PE2/com/stackroute/PE2/AggregateFinder.java
@@ -1,6 +1,48 @@
-package com.stackroute.PE2;
+package com.stackroute.PE2.com.stackroute.PE2;
 
 public class AggregateFinder  {
 
+//    to calculate average of inputs in an array
+    public int getAverage(int[] studentsgrades)
+    {
+//        initialization of variables
+        int average=0;
+        int total=0;
+        for(int i=0; i<studentsgrades.length; i++)
+        {
+         total= total+studentsgrades[i];
+         average= total/studentsgrades.length;
+        }
+        return average;
+    }
 
+//    to find the minimum integer in an array
+    public int getMinimum(int [] studentsgrades)
+    {
+//        initialization of variables
+        int minimum= studentsgrades[0];
+        for(int i=0; i<studentsgrades.length; i++)
+        {
+            if (studentsgrades[i]<minimum)
+            {
+                minimum= studentsgrades[i];
+            }
+        }
+
+        return minimum;
+    }
+//    to find the minimum integer in an array
+    public int getMaximum(int [] studentsgrades)
+    {
+//        initialization of variables
+        int maximum= studentsgrades[0];
+        for(int i=0; i<studentsgrades.length; i++)
+        {
+            if (studentsgrades[i]>maximum)
+            {
+                maximum= studentsgrades[i];
+            }
+        }
+        return maximum;
+    }
 }
